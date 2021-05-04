a93623f8da82778b28f2d3c34699f6e38de27628
vcamacho11 <rae.c1199@gmail.com>
2021-05-03 21:36:05 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('script loaded');
+$(document).ready(function(){
+    $('#show-me').click(function(){
+        $('#new-buttons').addClass('show');
+    });
+    $('#get-started').click(function(){
+        $('#blue-block').removeClass('bg-primary');
+        $('#blue-content').removeClass('text-white');
+    })
+    console.log('script loaded');
+})
